30c0da331ca77520230141c1c8a5bda9aefbaafb
viewpossibilities <viewpossibilities@gmail.com>
2023-04-04 04:55:16 -0700

    task 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,6 +5,7 @@ class Rectangle:
     """Define a class Rectangle."""
 
     def __init__(self, width=0, height=0):
+        """Define the width and Heigth"""
         self.width = width
         self.height = height
     
